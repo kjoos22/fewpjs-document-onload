e6067336ae09ad1d98c0305d45acf335e88a677e
kjoos22 <kjoos22@gmail.com>
2021-04-12 18:02:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+const text = document.getElementById("text")
+
+
+document.addEventListener("DOMContentLoaded", function() {    
+    
+    text.innerHTML = "This is really cool!"
+})
